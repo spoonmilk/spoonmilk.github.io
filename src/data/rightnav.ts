3545bf98196a0f33fdbe5520887cd3bcfa1a11e8
spoonmilk <haku31229@gmail.com>
2025-07-06 19:25:56 -0400

    Fix right nav scrolling issue

--- src/data/rightnav.ts
+++ src/data/rightnav.ts
@@ -1,60 +1,73 @@
 export const inspirations = [
-    // {
-    //     name: Alan Guth
+  // {
+  //     name: Alan Guth
 
-    // },
-    // Name: Maryam Mirzakahni
-    // Name: John Carmack
-    // Name: Alan Turing
-    // Name: Murray Gell-Mann
-    // Name: Claude Shannon
-    // Name: Paul Dirac
-    // Name: Parisa Safas
-    // Sean Carroll
-    // cre8math
-    // codeparade
-    {
-        name: "Karl van Bibber",
-        image: "vanbibber.jpg",
-        description: "I doubt he remembers me, but Professor van Bibber let me into his axion lab back in high school, thus putting me on the winding path towards computational physics.",
-        link: "https://nuc.berkeley.edu/people/karl-van-bibber/"
-    },
-    {
-        name: "Adrianne Lenker",
-        image: "adriannelenker.jpg",
-        alt: "Adrianne Lenker, a white woman wearing a cowboy hat and a tank top, leaning against a diner booth and looking at the camera.",
-        description: "My favorite songwriter, the reason I started playing guitar.",
-        link: "https://www.adriannelenker.com/"
-    },
-    {
-       name: "Joe Pera",
-        image: "joepera.jpg",
-        alt: "Joe Pera, a white man with glasses standing next to a red fire hydrant.",
-        description: "\'Joe Pera Talks with You\' is my current favorite show, and one that I truly believe nourishes whatever beating construct we call the soul. I would recommend anyone watch S1E3: Joe Pera Takes You on a Fall Drive.",
-        link: "https://archive.org/details/joe-pera-talks-with-you/Joe+Pera+Talks+With+You/Joe+Pera+Season+1/S1E3+Joe+Pera+Takes+You+on+a+Fall+Drive.mp4"
-    },
-    {
-        name: "Tim Rogers",
-        image: "timrogers.jpg",
-        alt: "A white man with scraggly hair, the games journalist Tim Rogers.",
-        description: "As much as I abhor medium, Tim Rogers' page contains multiple of my favorite pieces of writing on the internet.",
-        link: "https://medium.com/@108"
-    },
-    {
-        name: "Daniel Shiffman",
-        alt: "A photo Daniel Shiffman, creator of The Coding Train. He is a middle-aged white man with a dark, full beard and gentle smile. He is leaning against an old macintosh computer, looking at the photographer.",
-        image: "shiffman.jpg",
-        description: "I always seem to find myself back at the coding train as the prime example of great computer science education.",
-        link: "https://thecodingtrain.com"
-    },
-    {
-        name: "Chloe Qiao",
-        description: "The impetus for me to actually put this thing on the interwebs. Chloe is a great friend from university who also writes incredibly charming blog posts.",
-        link: "https://qiaochloe.com"
-    },
-    {
-        name: "Tanish Makadia",
-        description: "A beautiful website by a wonderful friend. If only I could be half as stylish.",
-        link: "https://tanishmakadia.com"
-    }
-]
+  // },
+  // Name: Maryam Mirzakahni
+  // Name: John Carmack
+  // Name: Alan Turing
+  // Name: Murray Gell-Mann
+  // Name: Claude Shannon
+  // Name: Paul Dirac
+  // Name: Parisa Safas
+  // Sean Carroll
+  // cre8math
+  // codeparade
+  {
+    name: "Karl van Bibber",
+    image: "vanbibber.jpg",
+    description:
+      "I doubt he remembers me, but Professor van Bibber let me into his axion lab back in high school, thus putting me on the winding path towards computational physics.",
+    link: "https://nuc.berkeley.edu/people/karl-van-bibber/",
+  },
+  {
+    name: "Adrianne Lenker",
+    image: "adriannelenker.jpg",
+    alt: "Adrianne Lenker, a white woman wearing a cowboy hat and a tank top, leaning against a diner booth and looking at the camera.",
+    description: "My favorite songwriter, the reason I started playing guitar.",
+    link: "https://www.adriannelenker.com/",
+  },
+  {
+    name: "Joe Pera",
+    image: "joepera.jpg",
+    alt: "Joe Pera, a white man with glasses standing next to a red fire hydrant.",
+    description:
+      "\'Joe Pera Talks with You\' is my current favorite show, and one that I truly believe nourishes whatever beating construct we call the soul. I would recommend anyone watch S1E3: Joe Pera Takes You on a Fall Drive.",
+    link: "https://archive.org/details/joe-pera-talks-with-you/Joe+Pera+Talks+With+You/Joe+Pera+Season+1/S1E3+Joe+Pera+Takes+You+on+a+Fall+Drive.mp4",
+  },
+  {
+    name: "Tim Rogers",
+    image: "timrogers.jpg",
+    alt: "A white man with scraggly hair, the games journalist Tim Rogers.",
+    description:
+      "As much as I abhor medium, Tim Rogers' page contains multiple of my favorite pieces of writing on the internet.",
+    link: "https://medium.com/@108",
+  },
+  {
+    name: "Debug debug",
+    alt: "A screenshot of the Debug debug website, which has a black background with white text and a red border. The text reads 'Debug debug' in large letters.",
+    description:
+      "A website that I stumbled upon while looking for a way to make my own website. It has a simple, clean design and is very easy to navigate.",
+    link: "https://debugdebug.com/",
+  },
+  {
+    name: "Daniel Shiffman",
+    alt: "A photo Daniel Shiffman, creator of The Coding Train. He is a middle-aged white man with a dark, full beard and gentle smile. He is leaning against an old macintosh computer, looking at the photographer.",
+    image: "shiffman.jpg",
+    description:
+      "I always seem to find myself back at the coding train as the prime example of great computer science education.",
+    link: "https://thecodingtrain.com",
+  },
+  {
+    name: "Chloe Qiao",
+    description:
+      "The impetus for me to actually put this thing on the interwebs. Chloe is a great friend from university who also writes incredibly charming blog posts.",
+    link: "https://qiaochloe.com",
+  },
+  {
+    name: "Tanish Makadia",
+    description:
+      "A beautiful website by a wonderful friend. If only I could be half as stylish.",
+    link: "https://tanishmakadia.com",
+  },
+];
