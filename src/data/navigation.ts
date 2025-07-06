9d7dc34d5c32b439ecda7955d5e5bf6b0bef72e0
spoonmilk <haku31229@gmail.com>
2025-07-06 00:24:15 -0400

    Add... everything

---
+++ src/data/navigation.ts
@@ -0,0 +1,53 @@
+export const pages = [
+    {
+        label: "Home",
+        href: "/",
+        description: "The homepage"
+    },
+    {
+        label: "About",
+        href: "/about",
+        description: "What you might have came for"
+    },
+    {
+        label: "Current Doings",
+        href: "/doing",
+        description: "A little stalking never hurt anybody."
+    },
+    {
+        label: "Past Doings",
+        href: "/dones",
+        description: "I think I've scared off the recruiters by now."
+    },
+    {
+        label: "Loves, Curiosities",
+        href: "/loves",
+        description: "There is something blossoming inside of me"
+    },
+];
+
+export const blogs = [
+    {
+        label: "This is gonna be an expandable dropdown",
+        href: "/blog/placeholder",
+        description: "There will be things here, one day."
+    }
+];
+
+export const links = [
+    {
+        label: "GitHub",
+        href: "https://github.com/spoonmilk",
+        description: "Questionably good code."
+    },
+    {
+        label: "LinkedIn",
+        href: "https://www.linkedin.com/in/",
+        description: "Professionalism eludes me."
+    },
+    {
+        label: "RSS",
+        href: "/rss.xml",
+        description: "Based."
+    }
+];
---
+++ src/data/rightnav.ts
@@ -0,0 +1,11 @@
+export const inspirations = [
+
+]
+
+export const collectedBlogs = [
+    {
+        name: "Tanish Makadia",
+        description: "A beautiful website by a wonderful friend",
+        link: "https://tanishmakadia.com"
+    }
+]
