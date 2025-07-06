52243735260f858d96207882689248d3755501d1
spoonmilk <haku31229@gmail.com>
2025-07-06 18:41:44 -0400

    update astro domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://spoonmilk.github.io',
+    site: 'https://spoonmilk.xyz',
 });
