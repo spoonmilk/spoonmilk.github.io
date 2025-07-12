804d37e495390c79082849c3b6f0f1040759ce6d
spoonmilk <haku31229@gmail.com>
2025-07-12 16:14:51 -0400

    Progress

--- src/data/leftnav.ts
+++ src/data/leftnav.ts
@@ -1,58 +1,53 @@
 export const pages = [
-    {
-        label: "Home",
-        href: "/",
-        description: "Where the heart is"
-    },
-    {
-        label: "About",
-        href: "/about",
-        description: "I'm an open book!"
-    },
-    {
-        label: "Current Doings",
-        href: "/now",
-        description: "A little stalking never hurt anybody."
-    },
-    {
-        label: "Past Doings",
-        href: "/projects",
-        description: "I think I've scared off the recruiters by now."
-    },
-    {
-        label: "Loves, Curiosities",
-        href: "/loves",
-        description: "There is something blossoming inside of me"
-    },
-    {
-        label: "Contacting Alex",
-        href: "/contact",
-        description: "Sometimes successful"
-    }
+  {
+    label: "Home",
+    href: "/",
+    description: "Where the heart is",
+  },
+  {
+    label: "About",
+    href: "/about",
+    description: "I'm an open book!",
+  },
+  {
+    label: "Current Doings",
+    href: "/now",
+    description: "A little stalking never hurt anybody.",
+  },
+  {
+    label: "Past Doings",
+    href: "/projects",
+    description: "Keeping logs",
+  },
+  {
+    label: "Loves, Curiosities",
+    href: "/loves",
+    description: "Little beautiful things",
+  },
 ];
 
 export const blogs = [
-    {
-        label: "Coming soon",
-        href: "/comingsoon",
-        description: "There will be things here, one day."
-    }
+  {
+    label: "Coming soon",
+    href: "/comingsoon",
+    description: "There will be things here, one day.",
+  },
 ];
 
 export const links = [
-    {
-        label: "GitHub",
-        href: "https://github.com/spoonmilk",
-        description: "Questionably good code."
-    },
-    {
-        label: "LinkedIn",
-        href: "https://www.linkedin.com/in/alexander-khosrowshahi-17a909298/",
-        description: "Professionalism eludes me."
-    },
-    {
-        label: "RSS",
-        href: "/comingsoon",
-        description: "Based."
-    }
+  {
+    label: "GitHub",
+    href: "https://github.com/spoonmilk",
+    description: "Questionably good code.",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/alexander-khosrowshahi-17a909298/",
+    description: "Professionalism eludes me.",
+  },
+  {
+    label: "RSS",
+    href: "/comingsoon",
+    description: "Based.",
+  },
 ];
