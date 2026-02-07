9c6834f19822ec1d1c941a45c7bda30fad5e48a9
spoonmilk <haku31229@gmail.com>
2026-02-07 13:56:42 -0500

    Bio change, primarily reformatting

--- src/data/leftnav.ts
+++ src/data/leftnav.ts
@@ -2,57 +2,57 @@ export const pages = [
     {
         label: "Home",
         href: "/",
-        description: "Where the heart is"
+        description: "Where the heart is",
     },
     {
         label: "About",
         href: "/about",
-        description: "I'm an open book!"
+        description: "I'm an open book!",
     },
     {
         label: "Current Doings",
         href: "/now",
-        description: "A little stalking never hurt anybody."
+        description: "A little stalking never hurt anybody.",
     },
     {
         label: "Past Doings",
         href: "/projects",
-        description: "I think I've scared off the recruiters by now."
+        description: "A sample of my non-scrapped projects.",
     },
     {
         label: "Loves, Curiosities",
         href: "/loves",
-        description: "There is something blossoming inside of me"
+        description: "There is something blossoming inside of me",
     },
     {
         label: "Contacting Alex",
         href: "/contact",
-        description: "Sometimes successful"
-    }
+        description: "Sometimes successful",
+    },
 ];
 
 export const blogs = [
     {
         label: "Coming soon",
         href: "/comingsoon",
-        description: "There will be things here, one day."
-    }
+        description: "There will be things here, one day.",
+    },
 ];
 
 export const links = [
     {
         label: "GitHub",
         href: "https://github.com/spoonmilk",
-        description: "Questionably good code."
+        description: "Questionably good code.",
     },
     {
         label: "LinkedIn",
         href: "https://www.linkedin.com/in/alexander-khosrowshahi-17a909298/",
-        description: "Professionalism eludes me."
+        description: "Professionalism eludes me.",
     },
     {
         label: "RSS",
         href: "/comingsoon",
-        description: "Based."
-    }
+        description: "Based.",
+    },
 ];
